cb4d1be1f2791cea17c71b2c01350f04cb85816e
viratpatel <viratrp@gmail.com>
2022-07-18 15:58:38 +0530

    Adding more test cases

--- src/utils.test.js
+++ src/utils.test.js
@@ -15,8 +15,19 @@ test('Should print 5 as Buzz', () => {
   expect(text).toBe('Buzz');
 });
 
+test('Should print 15 as FizzBuzz', () => {
+  const text = identifyNumber(15);
+  expect(text).toBe('FizzBuzz');
+});
+
 test("Should print [3,4,5] as ['Fizz, 4, Buzz']", () => {
   const values = fizzBuzz([3,4,5]);
   console.log(values);
   expect(values).toEqual(['Fizz', 4, 'Buzz']);
+});
+
+test("Should print [13,15,18] as ['13, FizzBuzz, Fizz']", () => {
+  const values = fizzBuzz([13,15,18]);
+  console.log(values);
+  expect(values).toEqual([13, 'FizzBuzz', 'Fizz']);
 });
